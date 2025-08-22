1df72e6bfc4e3ee8355b4d477093899440a5e9d0
Tu TA <>
2025-08-22 15:23:46 +0700

    [TUTA] update code

---
+++ projectTestPVCB/src/main/java/org/example/projecttestpvcb/ProjectTestPvcbApplication.java
@@ -0,0 +1,13 @@
+package org.example.projecttestpvcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTestPvcbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTestPvcbApplication.class, args);
+	}
+
+}
---
+++ projectTestPVCB/src/test/java/org/example/projecttestpvcb/ProjectTestPvcbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projecttestpvcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestPvcbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
